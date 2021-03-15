212690441b65b19ba19860586ea5865674a98575
HubbardHarvey3 <hubbard.harvey@gmail.com>
2021-03-15 19:15:15 -0400

    remove outdated AWS SDK

--- app.js
+++ app.js
@@ -2,59 +2,59 @@ var express = require("express")
 var app = express()
 var fs = require('fs')
 let ejs = require('ejs')
-var AWS = require('aws-sdk')
+// var AWS = require('aws-sdk')
 
 
 //AWS Stuff
-AWS.config.loadFromPath('./config.json')
-const s3 = new AWS.S3();
+// AWS.config.loadFromPath('./config.json')
+// const s3 = new AWS.S3();
 
 app.set('port', (process.env.PORT || 80))
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.render('../public/index.ejs')
 });
 
 
-app.get("/aws", function(req, res){
-    res.render('aws', {url:url})
-});
+// app.get("/aws", function(req, res){
+//     res.render('aws', {url:url})
+// });
 
 let url;
 
 //AWS Magic
 //For now, I am creating a single get request for each file.  Ideally, I would like to set the params.Key dynamically to cut down on the number of routes.
-//however, I don't know how to retrieve info from the front end to the server.
-app.get('/aws/1', function (req, res){
+// //however, I don't know how to retrieve info from the front end to the server.
+// app.get('/aws/1', function (req, res){
 
-    var params = {
-        Bucket: 'cbcsermons',
-        Key: '11-4-2018 AM Service.wav',
-        ResponseContentType: 'audio/wav',
-        ResponseContentDisposition: 'attachment'
-    };
-    
-    var url = s3.getSignedUrl('getObject', params);
-    res.render('file', {url:url})   
-})
+//     var params = {
+//         Bucket: 'cbcsermons',
+//         Key: '11-4-2018 AM Service.wav',
+//         ResponseContentType: 'audio/wav',
+//         ResponseContentDisposition: 'attachment'
+//     };
 
-app.get('/aws/2', function (req, res){
-    var params = {
-        Bucket: 'cbcsermons',
-        Key: '3-17-19 AM Service.wav',
-        ResponseContentType: 'audio/wav',
-        ResponseContentDisposition: 'attachment'
-    };   
-    var url = s3.getSignedUrl('getObject', params);
-    res.render('file', {url:url, params:params})   
-})
+//     var url = s3.getSignedUrl('getObject', params);
+//     res.render('file', {url:url})   
+// })
+
+// app.get('/aws/2', function (req, res){
+//     var params = {
+//         Bucket: 'cbcsermons',
+//         Key: '3-17-19 AM Service.wav',
+//         ResponseContentType: 'audio/wav',
+//         ResponseContentDisposition: 'attachment'
+//     };   
+//     var url = s3.getSignedUrl('getObject', params);
+//     res.render('file', {url:url, params:params})   
+// })
 
 
 //End of the Get requests for the files.
-app.get('/file', function(req, res){
-    res.render('file', {url:url})
+app.get('/file', function (req, res) {
+    res.render('file', { url: url })
 })
 
 
@@ -64,9 +64,9 @@ app.get('/file', function(req, res){
 
 
 // When I go live, this code needs to be used!!!!------------------------
- app.listen(app.get('port'), function() {
-   console.log("Node app is running at localhost:" + app.get('port'))
- })
+app.listen(app.get('port'), function () {
+    console.log("Node app is running at localhost:" + app.get('port'))
+})
 
 //app.listen(process.env.PORT, process.env.IP, function(){
 //   console.log("Blog server is spinning up");
